6428bacc2929e49783ff2f10f1f788282f8f7f72
tvs <teije-vs@hotmail.com>
2022-10-16 22:40:06 +0200

    feat: Add search for Boards and Projects

--- frontend/src/pages/Boards.svelte
+++ frontend/src/pages/Boards.svelte
@@ -1,11 +1,13 @@
 <script>
+    import {onMount} from "svelte";
+    import {Input} from "agnostic-svelte";
     import {GetBoards, GetBoardsCached} from "../../wailsjs/go/jirahelper/JiraHelper.js";
     import {callTracker, groupArr} from "./../util.js";
-    import {onMount} from "svelte";
     import Page from "../components/Page.svelte";
     import LoadingScreen from "./../components/LoadingScreen.svelte";
 
     let boards = []
+    let searchQuery = ""
 
     const retrieveBoards = async () => {
         boards = (await GetBoardsCached()) || []
@@ -21,12 +23,15 @@
 
 {#if boards.length > 0}
     <Page title="Boards" refresh={retrieveBoards}>
+        <Input type="text" placeholder="Search" bind:value={searchQuery}/>
         <section class="mbs16">
-            {#each groupArr(boards.sort((a, b) => a.name.localeCompare(b.name)), 2) as boardsWindow}
+            {#each groupArr(boards
+                .filter(b => b.name.toUpperCase().includes(searchQuery.toUpperCase()))
+                .sort((a, b) => a.name.localeCompare(b.name)), 2) as boardsWindow}
                 <div class="flex">
-                    <div class="column"><a href="/app/boards/{boardsWindow[0].id}">{boardsWindow[0].name}</a></div>
+                    <div class="column mbe4 mbs4"><a href="/app/boards/{boardsWindow[0].id}">{boardsWindow[0].name}</a></div>
                     {#if boardsWindow.length > 1}
-                        <div class="column"><a href="/app/boards/{boardsWindow[1].id}">{boardsWindow[1].name}</a></div>
+                        <div class="column mbe4 mbs4"><a href="/app/boards/{boardsWindow[1].id}">{boardsWindow[1].name}</a></div>
                     {/if}
                 </div>
             {/each}
--- frontend/src/pages/Projects.svelte
+++ frontend/src/pages/Projects.svelte
@@ -1,10 +1,12 @@
 <script>
     import {onMount} from "svelte";
+    import {Input} from "agnostic-svelte";
     import {callTracker, groupArr} from "./../util.js";
     import {RetrieveProjects, RetrieveProjectsCached} from "../../wailsjs/go/jirahelper/JiraHelper.js";
     import Page from "../components/Page.svelte";
 
     let projects = []
+    let searchQuery = ""
     onMount(async () => retrieveProjects())
 
     const retrieveProjects = async () => {
@@ -14,16 +16,21 @@
 </script>
 
 <Page title="Projects" refresh={retrieveProjects}>
-    <section class="mbs24">
-        {#each groupArr(projects.sort((a, b) => a.name.localeCompare(b.name)), 2) as projectsWindow}
+    <Input type="text" placeholder="Search" bind:value={searchQuery}/>
+    <section class="mbs16">
+        {#each groupArr(projects
+            .filter(p => p.name.toUpperCase().includes(searchQuery.toUpperCase()) || p.key.toUpperCase().includes(searchQuery.toUpperCase()))
+            .sort((a, b) => a.name.localeCompare(b.name)), 2) as projectsWindow}
             <div class="flex">
-                <div class="column">
-                    <img src="{projectsWindow[0].avatarUrls['48x48']}" height="24" width="24"/>
+                <div class="column mbe4 mbs4">
+                    <img src="{projectsWindow[0].avatarUrls['48x48']}" height="24" width="24"
+                         alt="avatar {projectsWindow[0].key}"/>
                     <a href="/app/projects/{projectsWindow[0].key}">{projectsWindow[0].name}</a>
                 </div>
                 {#if projectsWindow.length > 1}
-                    <div class="column">
-                        <img src="{projectsWindow[1].avatarUrls['48x48']}" height="24" width="24"/>
+                    <div class="column mbe4 mbs4">
+                        <img src="{projectsWindow[1].avatarUrls['48x48']}" height="24" width="24"
+                             alt="avatar {projectsWindow[1].key}"/>
                         <a href="/app/projects/{projectsWindow[1].key}">{projectsWindow[1].name}</a>
                     </div>
                 {/if}
