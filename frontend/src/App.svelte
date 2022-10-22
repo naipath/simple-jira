673ba07971aa49130e120736cad300b6ef582242
tvs <teije-vs@hotmail.com>
2022-10-22 17:38:20 +0200

    feat: Add active navigation style state

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,7 +1,7 @@
 <script>
     import {Header, HeaderNav, HeaderNavItem} from 'agnostic-svelte';
     import {onMount} from 'svelte';
-    import {Route, router} from 'tinro';
+    import {active, Route, router} from 'tinro';
     import {IsAuthenticated, LogOut, TryLoggingWithPreviousCredentials} from '../wailsjs/go/jirahelper/JiraHelper.js'
     import Boards from "./pages/Boards.svelte";
     import {LogPrint} from "../wailsjs/runtime/runtime.js";
@@ -22,11 +22,12 @@
         if (await IsAuthenticated()) {
             router.goto("/app")
             LogPrint("Authenticated with Jira")
-            return
+        } else {
+            router.goto("/login")
+            LogPrint("Unauthenticated with Jira")
         }
-        router.goto("/login")
-        LogPrint("Unauthenticated with Jira")
     })
+
     const logout = async () => {
         await LogOut()
         router.goto("/login")
@@ -54,32 +55,37 @@
 <Route path="/app/*">
     <Header isSticky>
         <HeaderNav>
-            <HeaderNavItem><a href="/app">Home</a></HeaderNavItem>
-            <HeaderNavItem><a href="/app/projects">Projects</a></HeaderNavItem>
-            <HeaderNavItem><a href="/app/boards">Boards</a></HeaderNavItem>
-            <HeaderNavItem><a href="/app/search-issues">Search issues</a></HeaderNavItem>
+            <HeaderNavItem><a href="/app/home" use:active>Home</a></HeaderNavItem>
+            <HeaderNavItem><a href="/app/projects" use:active>Projects</a></HeaderNavItem>
+            <HeaderNavItem><a href="/app/boards" use:active>Boards</a></HeaderNavItem>
+            <HeaderNavItem><a href="/app/search-issues" use:active>Search issues</a></HeaderNavItem>
         </HeaderNav>
         <div slot="logoright">
             <HeaderNavItem><a href="/logout" on:click={logout}>Logout</a></HeaderNavItem>
         </div>
     </Header>
-    <Route path="/">
+    <Route path="/" redirect="/app/home"/>
+    <Route path="/home">
         <Home/>
     </Route>
-    <Route path="/boards">
-        <Boards/>
-    </Route>
-    <Route path="/boards/:boardId" let:meta>
-        <BoardDetails boardId={meta.params.boardId}/>
+    <Route path="/boards/*">
+        <Route path="/">
+            <Boards/>
+        </Route>
+        <Route path="/:boardId" let:meta>
+            <BoardDetails boardId={meta.params.boardId}/>
+        </Route>
     </Route>
     <Route path="/search-issues">
         <SearchIssues/>
     </Route>
-    <Route path="/projects">
-        <Projects/>
-    </Route>
-    <Route path="/projects/:projectKey" let:meta>
-        <ProjectDetails projectKey={meta.params.projectKey}/>
+    <Route path="/projects/*">
+        <Route path="/">
+            <Projects/>
+        </Route>
+        <Route path="/:projectKey" let:meta>
+            <ProjectDetails projectKey={meta.params.projectKey}/>
+        </Route>
     </Route>
     <Route path="/issues/:issueKey" let:meta>
         <IssueDetails issueKey={meta.params.issueKey}/>
